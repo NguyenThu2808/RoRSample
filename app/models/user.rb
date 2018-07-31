3c40fbe71e5312fe1279dd310ee4ca4734a1ec80
NguyenThu2808 <nguyenthu280896@gmail.com>
2018-07-31 14:39:08 +0700

    Sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-  before_save { email.downcase! }
-  validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = Settings.valid_email_regex
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  before_save {email.downcase!}
+  validates :name, presence: true, length: {maximum: Settings.length_name}
+  VALID_EMAIL_REGEX = Settings.email_regex
+  validates :email, presence: true, length: {maximum: Settings.length_email},
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: Settings.pw_min}
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_28_034629) do
+ActiveRecord::Schema.define(version: 2018_08_01_031713) do
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "password_digest"
-    t.index ["email"], name: "index_users_on_email", unique: true
-  end
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'tring' for column 'reset_digest'
 
 end
